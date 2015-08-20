322df23f5980461d9e4ec737face976bfecfca5d
Simon Gibbon <srgibbon@googlemail.com>
2015-08-20 09:12:21 +0100

    Consolidation of temperature.lua
    Changes due to multiple temperature device types

--- lua/temperature.lua
+++ lua/temperature.lua
@@ -5,8 +5,11 @@ local http = require "socket.http";
 function get_temperature(DeviceName)
   idx = idx_from_name(DeviceName,'devices')
   if idx == nil then
-    return DeviceName, -999, -999, 0
+    return DeviceName, -999, -999, -999, 0
   end
+  Temperature = -999
+  Humidity = -999
+  Pressure = -999
 -- Determine temperature
   t = server_url.."/json.htm?type=devices&rid=" .. idx
   print ("JSON request <"..t..">");
@@ -14,22 +17,55 @@ function get_temperature(DeviceName)
   decoded_response = JSON:decode(jresponse)
   result = decoded_response["result"]
   record = result[1]
-  Temperature = record["Temp"]
-  Humidity = record["Humidity"]
+  DeviceType = record["Type"]
+  if DeviceType == "Temp" then
+    Temperature = record["Temp"]
+  else
+    if DeviceType == "Humidity" then
+      Humidity = record["Humidity"]
+    else
+      if DeviceType == "Temp + Humidity" then
+        Temperature = record["Temp"]
+        Humidity = record["Humidity"]
+      else
+        if DeviceType == "Temp + Humidity + Baro" then
+          Temperature = record["Temp"]
+          Humidity = record["Humidity"]
+          Pressure = record["Barometer"]
+        end
+      end
+    end
+  end
   LastUpdate = record["LastUpdate"]
   DeviceName = record["Name"]
-  return DeviceName, Temperature, Humidity, LastUpdate;
+  return DeviceName, Temperature, Humidity, Pressure, LastUpdate;
 end
 
 function temperature(DeviceName)
   local response = ""
-  DeviceName, Temperature, Humidity, LastUpdate = get_temperature(DeviceName)
-  if Temperature == -999 then
+  DeviceName, Temperature, Humidity, Pressure, LastUpdate = get_temperature(DeviceName)
+  if Temperature == -999 and Humidity == -999 and Pressure == -999 then
     print(DeviceName..' does not exist')
     return 1, DeviceName..' does not exist'
+  else
+    if Temperature == -999 and Pressure == -999 then
+      print(DeviceName .. ' relative humidity is ' .. Humidity .. '%')
+      response = DeviceName.. ' ' .. Humidity .. '%'
+    else  
+      if Pressure ~= -999 then
+        print(DeviceName .. ' temperature is ' .. Temperature .. '°C, relative humidity is ' .. Humidity .. '% and pressure is '.. Pressure..'hPa')
+        response = DeviceName.. ' ' .. Temperature .. '°C & ' .. Humidity .. '% & '.. Pressure .. 'hPa'
+      else
+        if Humidity ~= -999 then
+          print(DeviceName .. ' temperature is ' .. Temperature .. '°C and relative humidity is ' .. Humidity .. '%')
+          response = DeviceName.. ' ' .. Temperature .. '°C & ' .. Humidity .. '%'
+        else
+          print(DeviceName .. ' temperature is ' .. Temperature .. '°C')
+          response = DeviceName.. ' ' .. Temperature .. '°C'
+        end
+      end
+    end
   end
-  print(DeviceName .. ' temperature is ' .. Temperature .. '°C and relative humidity is ' .. Humidity .. '%')
-  response = DeviceName.. ' ' .. Temperature .. '°C & ' .. Humidity .. '%'
   return status, response;
 end
 
@@ -44,46 +80,24 @@ function temperature_module.handler(parsed_cli)
     status, response = temperature(DeviceName)
   else
     idx = idx_from_variable_name('DevicesWithTemperatures')
-    -- Get list of all user variables
---    t = server_url.."/json.htm?type=command&param=getuservariables"
---    print ("JSON request <"..t..">");  
---    jresponse, status = http.request(t)
---    decoded_response = JSON:decode(jresponse)
---    result = decoded_response["result"]
---    idx = 0
---    for k,record in pairs(result) do
---      if type(record) == "table" then
---        if record['Name'] == 'DevicesWithTemperatures' then
---          print(record['idx'])
---          idx = record['idx']
---        end
---      end
---    end
     if idx == 0 then
       print('User Variable DevicesWithTemperatures not set in Domoticz')
       return 1, 'User Variable DevicesWithTemperatures not set in Domoticz'
     end
     -- Get user variable DevicesWithTemperature
     DevicesWithTemperatures = get_variable_value(idx)
---    t = server_url.."/json.htm?type=command&param=getuservariable&idx="..idx
---    print ("JSON request <"..t..">");  
---    jresponse, status = http.request(t)
---    decoded_response = JSON:decode(jresponse)
---    result = decoded_response["result"]
---    record = result[1]
---    DevicesWithTemperatures = record["Value"]
---    DeviceNames = {}
     print(DevicesWithTemperatures)
---    for DeviceName in string.gmatch(DevicesWithTemperatures, "[^|]+") do
---      DeviceNames[#DeviceNames + 1] = DeviceName
---    end
     -- Retrieve the names
     DeviceNames = get_names_from_variable(DevicesWithTemperatures)
     -- Loop round each of the devices with temperature
-    response = ''
-    for i,DeviceName in ipairs(DeviceNames) do
-      status, r = temperature(DeviceName)
-      response = response .. r .. '\n'
+    if DeviceNames ~= nil then
+      response = ''
+      for i,DeviceName in ipairs(DeviceNames) do
+        status, r = temperature(DeviceName)
+        response = response .. r .. '\n'
+      end
+    else
+      response = 'No device names found in '..DevicesWithTemperatures
     end
   end
   return status, response
