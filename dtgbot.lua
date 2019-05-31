054316a28ebd80d9987f785171a76da006f17300
victordz <47621198+victordz@users.noreply.github.com>
2019-05-31 22:52:41 +0200

    Update dtgbot.lua

--- dtgbot.lua
+++ dtgbot.lua
@@ -115,6 +115,7 @@ http = require "socket.http";
 socket = require "socket";
 https = require "ssl.https";
 JSON = require "JSON";
+mime = require("mime")
 
 function file_exists(name)
    local f=io.open(name,"r")
